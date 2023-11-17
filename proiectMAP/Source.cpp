668b7d41fb185e19322368f689a0ecffa8b9c204
CincuIasmina <cnicoletaiasmina@gmail.com>
2023-11-17 17:46:32 +0200

    Am facut o functie care detecteaza daac regina este plasata pe un loc valid, adica verific daca nu exista o alta regina pe aceiasi linie sau diagonala.

--- proiectMAP/Source.cpp
+++ proiectMAP/Source.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 #define N 8
 
+bool nuAtaca(int board[][N], int linie, int col) {
+    for (int i = 0; i < col; i++) 
+        if (board[linie][i] == 1)
+            return false;
+    for (int i = linie, j = col; i >= 0 && j >= 0; i--, j--) { 
+            return false;
+    }
+    for (int i = linie, j = col; i < N && j >= 0; i++, j--) 
+        if (board[i][j] == 1)
+            return false;
+    return true;
+}
+
 int main()
 {
     int tabla[N][N];

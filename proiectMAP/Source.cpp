4610939bb414951e673aba19cf7fefaaa1a16a7a
CincuIasmina <cnicoletaiasmina@gmail.com>
2023-11-17 17:57:36 +0200

    Am adaugat logica pentru rezolvarea tablei de sah, respectiv plasarea reginelor.

--- proiectMAP/Source.cpp
+++ proiectMAP/Source.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 #define N 8
 
+int solutii = 0;
+
 bool nuAtaca(int board[][N], int linie, int col) {
     for (int i = 0; i < col; i++) 
         if (board[linie][i] == 1)
@@ -17,6 +19,28 @@ bool nuAtaca(int board[][N], int linie, int col) {
     return true;
 }
 
+bool rezolvaTabla(int tabla[][N], int col) {
+    if (col == N) { //daca a ajuns la ultima coloana printeaza tabla
+        cout << "-------------------------------" << endl;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                cout << tabla[i][j] << " ";
+            cout << endl;
+        }
+        cout << "-------------------------------" << endl;
+        solutii++;
+    }
+    for (int i = 0; i < N; i++) {
+        if (nuAtaca(tabla, i, col)) {
+            tabla[i][col] = 1;
+            if (rezolvaTabla(tabla, col + 1))
+                return true;
+            tabla[i][col] = 0;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     int tabla[N][N];
@@ -24,5 +48,7 @@ int main()
         for (int j = 0; j < N; j++)
             tabla[i][j] = 0;
     }
+    rezolvaTabla(tabla, 0);
+    cout << "Numarul de solutii este: " << solutii;
 	return 0;
 }

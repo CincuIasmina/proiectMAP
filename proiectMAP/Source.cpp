b7360d5bfe4bc7752da1fd7e235ae395c1e0c7a0
CincuIasmina <cnicoletaiasmina@gmail.com>
2023-11-17 17:33:02 +0200

    Am facut tabla de sah, respectiv o matrice de 8 pe 8 si care este initializata cu 0.

--- proiectMAP/Source.cpp
+++ proiectMAP/Source.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+#define N 8
 
 int main()
 {
-	cout << "Hello world!";
+    int tabla[N][N];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            tabla[i][j] = 0;
+    }
 	return 0;
 }
